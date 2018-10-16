bc3c79d7e67f0b15930c5f8fd64c7ad6c7f49f18
Luke Melaia <lukemelaia.personal@gmail.com>
2018-10-16 19:28:36 +0200

    Introduction paragraph now scales right on small mobiles.

--- js/index.js
+++ js/index.js
@@ -4,10 +4,19 @@ $(window).resize(function() {
     } else {
         $("#forge-frame").show();
     }
+    
+    if($(window).width() < 800){
+        $("#introduction").removeClass("it");
+        $("#introduction").addClass("it-mobile");
+    } else {
+        $("#introduction").removeClass("it-mobile");
+        $("#introduction").addClass("it");
+    }
 });
 
 $(document).ready(function() {
     if($(window).width() < 1000){
         $("#forge-frame").hide();
     }
+    
 });
