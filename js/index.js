2415b5c6b5bf8d2cd85126dae7905a3561fb8644
Luke Melaia <lukemelaia.personal@gmail.com>
2018-10-17 01:11:51 +0200

    Second fix for images on mobile

--- js/index.js
+++ js/index.js
@@ -8,9 +8,15 @@ $(window).resize(function() {
     if($(window).width() < 800){
         $("#introduction").removeClass("it");
         $("#introduction").addClass("it-mobile");
+        $("#img-0").addClass("image-mobile");
+        $("#img-1").addClass("image-mobile");
+        $("#img-2").addClass("image-mobile");
     } else {
         $("#introduction").removeClass("it-mobile");
         $("#introduction").addClass("it");
+        $("#img-0").removeClass("image-mobile");
+        $("#img-1").removeClass("image-mobile");
+        $("#img-2").removeClass("image-mobile");
     }
 });
 
@@ -22,8 +28,14 @@ $(document).ready(function() {
     if($(window).width() < 800){
         $("#introduction").removeClass("it");
         $("#introduction").addClass("it-mobile");
+        $("#img-0").addClass("image-mobile");
+        $("#img-1").addClass("image-mobile");
+        $("#img-2").addClass("image-mobile");
     } else {
         $("#introduction").removeClass("it-mobile");
         $("#introduction").addClass("it");
+        $("#img-0").removeClass("image-mobile");
+        $("#img-1").removeClass("image-mobile");
+        $("#img-2").removeClass("image-mobile");
     }
 });
